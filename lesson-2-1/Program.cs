f34c6977b00ce0eec9a9841fa49fba76d3af616f
lameR <erpresents@gmail.com>
2021-02-22 17:43:34 +0300

    Create lesson-2-1

---
+++ lesson-2-1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+
+namespace lesson_2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Consol();
+        }
+        private static void Consol()
+        {
+            try
+            {
+                Console.Write("Введите минимальную температуру за сутки: ");
+                double readMinTp = ReadTemp();
+                Console.Write("Введите максимальную температуру за сутки: ");
+                double readMaxTp = ReadTemp();
+                Console.WriteLine();
+                Console.WriteLine($"Среднесуточная температура за сутки: {Math.Round((readMaxTp % readMinTp), MidpointRounding.AwayFromZero).ToString().Replace(",", ".")}");
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Error(ex.Message);
+            }
+        }
+        private static double ReadTemp()
+        {
+            var tp = Convert.ToDouble(Console.ReadLine().Replace(".", ","));
+            return tp;
+        }
+        private static void Error(string ex)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(ex);
+            Thread.Sleep(2000);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Clear();
+            Consol();
+        }
+    }
+}
