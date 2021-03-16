223b4059f78d6b94d85811c997eed9fc671458c5
lameR <erpresents@gmail.com>
2021-03-16 04:07:08 +0300

    Create lesson-6

---
+++ lesson-6/ArgumentsList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson_6
+{
+    internal class ArgumentsList
+    {
+        protected static void GetList()
+        {
+            Console.WriteLine("Parameter List:");
+            Console.WriteLine("{0,5}{1,0}{2,43}", "", "/TASKLIST", "Displaying a list of processes");
+            Console.WriteLine("{0,5}{1,0}{2,43}", "", "/TASKLIST /F", "search process by name");
+            Console.WriteLine("{0,5}{1,0}{2,38}", "", "/TASKKILL", "Completion of the process");
+            Console.WriteLine("{0,5}{1,0}{2,41}", "", "/TASKKILL /PID", "Termination of the process by PID");
+            Console.WriteLine("{0,5}{1,0}{2,40}", "", "/TASKKILL /IMAGENAME", "Completion of the process by IMAGENAME");
+        }
+    }
+}
---
+++ lesson-6/TaskList.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson_6
+{
+    internal class TaskList : ArgumentsList
+    {
+        public TaskList(string[] args)
+        {
+            try
+            {
+                if (args.Length == 1) GetProcessList();
+                else if (args.Length == 2 || args[1].ToUpper() == "/F") GetProcessList(args[2]);
+                else MethodErrors.Error(new  ArgumentException("invalid argument!"));
+            }
+            catch (Exception e)
+            {
+                MethodErrors.Error(e);
+            }
+        }
+        private void GetProcessList()
+        {
+            try
+            {
+                var processes = Process.GetProcesses();
+                Rendering(processes);
+            }
+            catch (Exception e)
+            {
+                MethodErrors.Error(e);
+            }
+        }
+
+        private void GetProcessList(string argument)
+        {
+            try
+            {
+                var processes = Process.GetProcessesByName(argument);
+                Rendering(processes);
+            }
+            catch (Exception e)
+            {
+                MethodErrors.Error(e);
+            }
+        }
+
+        private static void Rendering(Process[] processes)
+        {
+            try
+            {
+                Console.WriteLine("{0,-20}{1,14}{2,12}{3,13}", "Image Name", "PID", "Session#", "Mem Usage");
+                Console.WriteLine("{0,-20}{1,9}{2,12}{3,13}", "=========================", "========", "===========", "============");
+                foreach (var process in processes)
+                {
+                    var processName = process.ProcessName.Length > 20 ? process.ProcessName.Remove(20) : process.ProcessName;
+                    Console.WriteLine("{0,-20}{1,14}{2,12}{3,13}", processName, process.Id, process.SessionId, process.PeakPagedMemorySize64);
+                }
+            }
+            catch (Exception e)
+            {
+                MethodErrors.Error(e);
+            }
+        }
+    }
+}
