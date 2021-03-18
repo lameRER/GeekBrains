5277cdc5e05175e2941c38a5513ecc7a507b5406
lameR <erpresents@gmail.com>
2021-03-18 04:25:38 +0300

    lession-7 Decompilation

---
+++ lesson-7/Decopilation/lesson-7/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("lesson-7-1")]
+[assembly: AssemblyConfiguration("Debug")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyProduct("lesson-7-1")]
+[assembly: AssemblyTitle("lesson-7-1")]
+[assembly: AssemblyVersion("1.0.0.0")]
---
+++ lesson-7/Decopilation/lesson-7/Program.cs
@@ -0,0 +1,22 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: lesson_7_1.Program
+// Assembly: lesson-7-1, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: C9A3BD5C-87CF-4393-853E-0F796E6D1BBA
+// Assembly location: C:\Users\Sasha\source\repos\HomeWork\lesson-7\bin\Debug\netcoreapp3.1\lesson-7-1.dll
+
+using System;
+using System.IO;
+
+namespace lesson_7_1
+{
+  internal class Program
+  {
+    private static void Main(string[] args)
+    {
+            Console.WriteLine("Decopilation");
+      foreach (string directory in Directory.GetDirectories("C:\\"))
+        Console.WriteLine(directory);
+      Console.ReadLine();
+    }
+  }
+}
