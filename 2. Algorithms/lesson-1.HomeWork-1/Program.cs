812a2da8f60b17365f0b6d3d7df1a2535fa4046c
lameR <erpresents@gmail.com>
2021-04-21 03:53:08 +0300

    Create lesson-1.HomeWork-1

---
+++ 2. Algorithms/lesson-1.HomeWork-1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace lesson_1.HomeWork_1
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            var testCase1 = new TestCase { Number = 5, Expected = "Простое" };
+            var testCase2 = new TestCase { Number = 24, Expected = "Не простое" };
+            var testCase3 = new TestCase { Number = 79, Expected = "Простое" };
+            var testCase4 = new TestCase { Number = -782, Expected = "" };
+            var testCase5 = new TestCase { Number = -155, Expected = "" };
+
+            TestCase.Test(testCase1);
+            TestCase.Test(testCase2);
+            TestCase.Test(testCase3);
+            TestCase.Test(testCase4);
+            TestCase.Test(testCase5);
+            Console.ReadKey();
+        }
+
+        public static string Function(int number)
+        {
+            var d = 0;
+            var i = 2;
+            while (i < number)
+            {
+                if (number % i == 0)
+                    d++;
+                i++;
+            }
+
+            return d == 0 ? "Простое" : "Не простое";
+        }
+    }
+
+    public class TestCase
+    {
+        public int Number { get; set; }
+        public string Expected { get; set; }
+
+        public static void Test(TestCase test)
+        {
+            var result = Program.Function(test.Number);
+            Console.WriteLine(result == test.Expected ? "Valid test" : "Not valid test");
+        }
+    }
+}
