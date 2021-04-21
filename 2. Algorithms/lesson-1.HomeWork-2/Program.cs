700320f68078c1a709fa5ffc4c851241b248213e
lameR <erpresents@gmail.com>
2021-04-21 05:48:19 +0300

    Create lesson-1.HomeWork-2

---
+++ 2. Algorithms/lesson-1.HomeWork-2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace lesson_1.HomeWork_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public static int StrangeSum(int[] inputArray)
+        {
+            int sum = 0;                                            //O(1)
+
+            for (int i = 0; i < inputArray.Length; i++)             //O(inputArray.Length)
+            {
+                for (int j = 0; j < inputArray.Length; j++)         //O(inputArray.Length)
+                {
+                    for (int k = 0; k < inputArray.Length; k++)     //O(inputArray.Length)
+                    {
+                        int y = 0;                                  
+
+                        if (j != 0)
+                        {
+                            y = k / j;                              
+                        }
+
+                        sum += inputArray[i] + i + k + j + y;       
+                    }
+                }
+            }
+
+            return sum;                                             //O(1)
+        }
+
+        // O(2) + O(inputArray.Length * inputArray.Length * inputArray.Length)
+    }
+}
