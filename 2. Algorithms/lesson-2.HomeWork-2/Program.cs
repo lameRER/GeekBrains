f7f8f119a270c9dd50cf05d019fb5a998bbc4a5b
lameR <erpresents@gmail.com>
2021-04-25 15:51:49 +0300

    Create lesson-2.HomeWork-2

---
+++ 2. Algorithms/lesson-2.HomeWork-2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace lesson_2.HomeWork_2
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var ran = new Random();
+            const int n = 10000000;
+            var arr = new int[n];
+            for (var i = 0; i < arr.Length; i++) arr[i] = ran.Next(1, n);
+            Array.Sort(arr);
+            for (var i = 0; i < 40; i++)
+            {
+                var (index, countIteration) = BinarySearch(arr, ran.Next(1, n));
+                Console.WriteLine(index == -1
+                    ? $"Значение {ran.Next(1, n)} не найдено, количество интеграций: {countIteration}"
+                    : $"Значение {ran.Next(1, n)}, индекс: {index}, количество интеграций: {countIteration}");
+            }
+            
+        }
+
+        private static (int index, int countIteration) BinarySearch(int[] inputArray, int searchValue)
+        {
+            var countIteration = 0;
+            var min = 0;
+            var max = inputArray.Length - 1;
+            while (min <= max)
+            {
+                var mid = (min + max) / 2;
+                if (searchValue == inputArray[mid])
+                    return (mid, countIteration);
+                else if (searchValue < inputArray[mid])
+                    max = mid - 1;
+                else
+                    min = mid + 1;
+                countIteration++;
+            }
+            return (-1, countIteration);
+        }
+    }
+}
