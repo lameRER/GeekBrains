44f4d57769336429fa7ecce780ed986f6e54f17b
lameR <erpresents@gmail.com>
2021-04-25 22:58:27 +0300

    Create lesson-4.HomeWork-1

---
+++ 2. Algorithms/lesson-2.HomeWork-4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lesson_2.HomeWork_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
