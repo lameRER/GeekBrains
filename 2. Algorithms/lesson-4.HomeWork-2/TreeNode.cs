3369e9729c854bdf85630cee856ea15c18b32a13
lameR <erpresents@gmail.com>
2021-05-04 00:39:23 +0300

    Create lesson-4.Homework-2

---
+++ 2. Algorithms/lesson-4.HomeWork-2/TreeNode.cs
@@ -0,0 +1,19 @@
+namespace lesson_4.HomeWork_2
+{
+    public class TreeNode
+    {
+        public int Value { get; set; }
+        public TreeNode LeftChild { get; set; }
+        public TreeNode RightChild { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            var node = obj as TreeNode;
+
+            if (node == null)
+                return false;
+
+            return node.Value == Value;
+        }
+    }
+}
