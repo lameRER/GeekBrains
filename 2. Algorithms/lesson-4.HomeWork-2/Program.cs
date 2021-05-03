9c8af00c820084314c1ca49444a283dd24f0245c
lameR <erpresents@gmail.com>
2021-05-04 00:39:23 +0300

    Create lesson-4.Homework-2

---
+++ 2. Algorithms/lesson-4.HomeWork-2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using lesson_4.HomeWork.Library;
+
+namespace lesson_4.HomeWork_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] values = { 16, 8, 4, 2, 1, 3, 6, 5, 7, 12, 10, 9, 11, 14, 13, 15, 24, 20, 18, 17, 19, 22, 21, 23, 28, 26, 25, 27, 30, 29, 31 };
+            var ltree = new TreeNode();
+            foreach (var item in values)
+            {
+                ltree.AddItem(item);
+            }
+            
+            var node = ltree.GetNodeByValue(28);
+            if (node != null)
+                Console.WriteLine($"\nFound an element with the value {node.Value}");
+            
+            ltree.PrintTree();
+            ltree.RemoveItem(8);
+            ltree.RemoveItem(20);
+            ltree.RemoveItem(28);
+            Console.Clear();
+            ltree.PrintTree();
+            
+            Console.ReadKey();
+        }
+        
+    }
+}
