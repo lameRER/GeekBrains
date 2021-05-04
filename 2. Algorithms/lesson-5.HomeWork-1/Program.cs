949b0e56c6130ca425149c3a04230ef34197dfb7
lameR <erpresents@gmail.com>
2021-05-04 18:00:14 +0300

    Create lesson-5.Homework

---
+++ 2. Algorithms/lesson-5.HomeWork-1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using lesson_4.HomeWork.Library;
+
+namespace lesson_5.HomeWork_1
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            int[] values =
+            {
+                16, 8, 4, 2, 1, 3, 6, 5, 7, 12, 10, 9, 11, 14, 13, 15, 24, 20, 18, 17, 19, 22, 21, 23, 28, 26, 25,
+                27, 30, 29, 31
+            };
+            var lNode = new TreeNode();
+            foreach (var item in values) lNode.AddItem(item);
+            lNode.PrintTree();
+            OutConsole("BFS: ", lNode.BreadthFirstSearch());
+            OutConsole("DFS: ", lNode.DeepFirstSearch());
+            Console.ReadKey();
+        }
+
+        private static void OutConsole(string text, IEnumerable<TreeNode> firstSearch)
+        {
+            Console.WriteLine();
+            Console.Write(text);
+            foreach (var item in firstSearch) Console.Write("{0}, ", item.Value);
+            Console.WriteLine();
+        }
+    }
+}
