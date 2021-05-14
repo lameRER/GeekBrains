d4bee9067f2e42313d71ad3e67c8772452141550
lameR <erpresents@gmail.com>
2021-05-14 18:51:08 +0300

    Create lesson-8.Homework

---
+++ 2. Algorithms/lesson-4.HomeWork.Library/Sort/AlgorithmBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lesson_4.HomeWork.Library.Sort
+{
+    public class AlgorithmBase<T> where T: IComparable
+    {
+        public List<T> Items { get; set; } = new List<T>();
+        public int SwopCount { get; protected set; } = 0;
+        public int ComparisonCount { get; protected set; } = 0;
+
+        protected void Swop(int positionA, int positionB)
+        {
+            if (positionA >= Items.Count || positionB >= Items.Count) return;
+            var temp = Items[positionA];
+            Items[positionA] = Items[positionB];
+            Items[positionB] = temp;
+            SwopCount++;
+        }
+
+        public virtual void Sort()
+        {
+            SwopCount = 0;
+            Items.Sort();
+        }
+    }
+}
---
+++ 2. Algorithms/lesson-8.HomeWork/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using lesson_4.HomeWork.Library;
+using lesson_4.HomeWork.Library.Sort;
+
+namespace lesson_8.HomeWork
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            var rnd = new Random();
+            var items = new List<int>();
+            for (int i = 0; i < 20; i++)
+            {
+                items.Add(rnd.Next(0,100));
+            }
+            foreach (var item in items)
+            {
+                Console.Write("{0} ", item);
+            }
+
+            GetSort("BubbleSort", new BubbleSort<int>(), items);
+            GetSort("CocktailSort", new CocktailSort<int>(), items);
+        }
+
+        private static void GetSort(string sortName, AlgorithmBase<int> baseSort, IEnumerable<int> items)
+        {
+            var stopwatch = new Stopwatch();
+            baseSort.Items.AddRange(items);
+            stopwatch.Start();
+            baseSort.Sort();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("{0}: SwopCount:{2} {1}", sortName, stopwatch.Elapsed, baseSort.SwopCount);
+            stopwatch.Stop();
+            foreach (var item in baseSort.Items)
+            {
+                Console.Write("{0} ", item);
+            }
+        }
+    }
+}
