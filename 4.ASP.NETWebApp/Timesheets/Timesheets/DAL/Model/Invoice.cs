c5c01ab636dc8453083e0870ef9a912785c6abdc
lameR <erpresents@gmail.com>
2021-08-13 00:18:38 +0300

    Modify models

--- 4.ASP.NETWebApp/Timesheets/Timesheets/DAL/Model/Invoice.cs
+++ 4.ASP.NETWebApp/Timesheets/Timesheets/DAL/Model/Invoice.cs
@@ -1,17 +1,27 @@
 using System;
+using System.Collections.Generic;
 
 namespace Timesheets.DAL.Model
 {
     public class Invoice
     {
-        public int ContractId { get; set;}
-
-        public int EmployeeId { get; set;}
+        public Invoice()
+        {
+            InvoiceTasksCollection = new HashSet<InvoiceTask>();
+        }
+        
+        public int Id { get; set;}
+        
+        public int ContractId { get; set; }
 
         public DateTime Date { get; set;}
 
         public decimal Total { get; set;}
 
         public string Description { get; set;}
+        
+        public virtual Contract Contract { get; set; }
+        
+        public virtual ICollection<InvoiceTask> InvoiceTasksCollection { get; set; }
     }
 }
---
+++ 4.ASP.NETWebApp/Timesheets/Timesheets/DAL/Model/TaskEmployee.cs
@@ -0,0 +1,17 @@
+namespace Timesheets.DAL.Model
+{
+    public class TaskEmployee
+    {
+        public int Id { get; set;}
+        
+        public int TaskId { get; set;}
+        
+        public int EmployeeId { get; set;}
+        
+        public int TimeSpent { get; set;}
+        
+        public virtual Task Task { get; set; }
+        
+        public virtual Employee Employee { get; set; }
+    }
+}
