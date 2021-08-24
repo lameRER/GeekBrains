0c1a2ab08997a45405bf1a75001c1d9df361c9f2
lameR <erpresents@gmail.com>
2021-08-25 02:55:42 +0300

    Rename AddCustomerQuery to AddCustomerInsert

--- 4.ASP.NETWebApp/Timesheets/Timesheets/Request/AddCustomerInsert.cs
+++ 4.ASP.NETWebApp/Timesheets/Timesheets/Request/AddCustomerInsert.cs
@@ -2,28 +2,30 @@ using System.Threading;
 using System.Threading.Tasks;
 using AutoMapper;
 using MediatR;
+using Microsoft.AspNetCore.Mvc;
 using Timesheets.DAL.Interfaces;
 using Timesheets.DAL.Models;
 using Timesheets.Responses;
 
 namespace Timesheets.Request
 {
-    public class AddCustomerQuery : IRequest<CustomerDto>
+    public class AddCustomerInsert : IRequest<CustomerDto>
     {
+        [FromBody]
         public string Name { get; set; }
-        
-        public class AddCustomerQueryHander : IRequestHandler<AddCustomerQuery, CustomerDto>
+
+        public class AddCustomerInsertHandler : IRequestHandler<AddCustomerInsert, CustomerDto>
         {
             private readonly ICustomerRepository _repository;
             private readonly IMapper _mapper;
 
-            public AddCustomerQueryHander(ICustomerRepository repository, IMapper mapper)
+            public AddCustomerInsertHandler(ICustomerRepository repository, IMapper mapper)
             {
                 _repository = repository;
                 _mapper = mapper;
             }
 
-            public async Task<CustomerDto> Handle(AddCustomerQuery query, CancellationToken cancellationToken)
+            public async Task<CustomerDto> Handle(AddCustomerInsert query, CancellationToken cancellationToken)
             {
                 var customer = await _repository.Create(_mapper.Map<Customer>(query));
                 return _mapper.Map<CustomerDto>(customer);
