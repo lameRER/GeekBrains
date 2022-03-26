179be43c1991afc707dae43caa30451842f5d4d8
lameR <erpresents@gmail.com>
2022-03-27 00:28:19 +0300

    Task 1. Done

---
+++ 7.ASP.NETWebAppMVC/lesson-1/Task-1-2/WpfAppTask-1-2/WpfAppTask-1-2/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading;
+using System.Windows;
+
+namespace WpfAppTask_1_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private int Fibonachi(int value)
+        {
+            if (value is 0 or 1) return value;
+
+            return Fibonachi(value - 1) + Fibonachi(value - 2); 
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonStart.IsEnabled = false;
+            var thread = new Thread(() =>
+            {
+                for (int i = 0; i < 20; i++)
+                {
+                    var result = Fibonachi(i);
+                    TextBlockFibonachi.Dispatcher.Invoke(() =>
+                    {
+                        TextBlockFibonachi.Text = result.ToString();
+                    });
+                    Thread.Sleep(1000);
+                }
+                ButtonStart.Dispatcher.Invoke(() =>
+                {
+                    ButtonStart.IsEnabled = true;
+                });
+            });
+            thread.Start();
+        }
+    }
+}
