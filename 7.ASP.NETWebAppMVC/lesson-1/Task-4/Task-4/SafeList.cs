d2f2c8f8be842960f897d191d6aa2188696d8c29
lameR <erpresents@gmail.com>
2022-03-28 18:25:28 +0300

    Task 4. Done

---
+++ 7.ASP.NETWebAppMVC/lesson-1/Task-4/Task-4/SafeList.cs
@@ -0,0 +1,23 @@
+﻿namespace Task_4;
+
+public class SafeList<T>
+{
+    private readonly List<T> _list = new();
+    private readonly object _lockObject = new();
+    
+    public void Add(T item)
+    {
+        lock (_lockObject)
+        {
+            _list.Add(item);        
+        }
+    }
+
+    public void Remove(T item)
+    {
+        lock (_lockObject)
+        {
+            _list.Remove(item);
+        }
+    }
+}
