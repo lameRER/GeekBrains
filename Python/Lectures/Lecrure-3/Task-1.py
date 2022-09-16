445e2ba3b83db2be5a9a50d969768e4e72c7074c
lameR <32497792+lameRER@users.noreply.github.com>
2022-09-16 16:29:56 +0300

    Update Task-1.py

--- Python/Lectures/Lecrure-3/Task-1.py
+++ Python/Lectures/Lecrure-3/Task-1.py
@@ -1,7 +1,2 @@
 lst = [int(i) for i in input('Введите список чисел: ').split()]
-lstSum = 0
-for k, l in enumerate(lst):
-    if k % 2:
-        lstSum += l
-print(lstSum)
-        
+print(sum(lst[1::2]))
