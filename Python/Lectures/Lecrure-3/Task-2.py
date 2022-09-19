d6732290361a941d466d101409e9def162e285f7
lameR <erpresents@gmail.com>
2022-09-19 21:25:08 +0300

    Fix Task-2

--- Python/Lectures/Lecrure-3/Task-2.py
+++ Python/Lectures/Lecrure-3/Task-2.py
@@ -2,6 +2,6 @@ import math
 
 lst = [int(i) for i in input('Введите список чисел: ').split()]
 multi = []
-for l in range(math.ceil(len(lst) / 2)):
+for ls in range(math.ceil(len(lst) / 2)):
     multi.append(lst[l] * lst[-l - 1])
 print(lst, '=', multi)
