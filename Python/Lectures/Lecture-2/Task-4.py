6963ab338384b56b1628a5afcad42db262957e26
lameR <erpresents@gmail.com>
2022-09-19 21:00:49 +0300

    Fix Task-4

--- Python/Lectures/Lecture-2/Task-4.py
+++ Python/Lectures/Lecture-2/Task-4.py
@@ -4,21 +4,18 @@ work = 1
 lists = [-4, -3, -2, -1, 0, 1, 2, 3, 4]
 
 
-
 def SetFile(lists, path):
     for ls in lists:
         with open(path, 'a') as f:
             f.write(str(f'{ls}\n'))
 
+
 def GetFileIndex(digit, path):
-    
     with open(path, 'r') as f:
         fl = f.readlines()
     return int(fl[digit])
 
 
-
 digits = input("Вветите список чисел").split(' ')
 
 SetFile(lists, pth)
-
