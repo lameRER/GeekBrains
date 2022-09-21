bc08009573693d6f87bcdea6e81f614f91f7eb1d
lameR <32497792+lameRER@users.noreply.github.com>
2022-09-21 10:00:54 +0300

    Create Task-1.py

--- Python/Lectures/Lecture-4/Task-3.py
+++
@@ -1,9 +0,0 @@
-digitList = [1, 23, 5, 3465, 476, 878, 769, 8797, 1, 3, 4, 5, 66, 75, 55, 77, 88, 88, 11]
-unique = []
-for d in digitList:
-    if d in unique:
-        continue
-    else:
-        unique.append(d)
-print(digitList)
-print(unique)
